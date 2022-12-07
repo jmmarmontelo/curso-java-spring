d8dbe6d949daa871bd333a022b15afa0ebd12993
Joao Marcos <jmmarmontelo@gmail.com>
2022-12-07 18:30:21 -0300

    inicializacao do projeto

---
+++ curso-spring/src/main/java/com/curso/cursospring/HelloController.java
@@ -0,0 +1,20 @@
+package com.curso.cursospring;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/hello")
+    public Hello hello(@RequestParam(value = "name", defaultValue = "world") String name){
+        return new Hello(counter.incrementAndGet(), String.format(template, name));
+    }
+    
+}
